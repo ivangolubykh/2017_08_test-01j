b9ac27b7d8b1604ce8539c5a2941c9ae5a870d3c
ivangolubykh <ivan9@allworld.xyz>
2017-08-25 18:25:17 +0300

    Настроена передача данных из формы добавления задач, включая проверку csrf-токена.

--- task_list/app_task_list/static/js/task_list_ctrl.js
+++ task_list/app_task_list/static/js/task_list_ctrl.js
@@ -1,25 +1,29 @@
 'use strict';
-var TaskLstApp = angular.module('TaskLstApp', []);
 
-TaskLstApp.controller('TaskListCtrl', function TaskLstController($scope, $http) {
+
+var TaskListApp = angular.module('TaskListApp', ['ngCookies']);
+TaskListApp.controller('TaskListCtrl', function TaskListController($scope, $http) {
   $http.get('/get_data_json/').then(function(response) {
-        $scope.data_list = response.data.list;
-      });
-
-
-//  $scope.phones = [
-//    {
-//      name: 'Nexus S',
-//      snippet: 'Fast just got faster with Nexus S.'
-//    }, {
-//      name: 'Motorola XOOM™ with Wi-Fi',
-//      snippet: 'The Next, Next Generation tablet.'
-//    }, {
-//      name: 'MOTOROLA XOOM™',
-//      snippet: 'The Next, Next Generation tablet.'
-//    }
-//  ];
-//  $scope.hello = "Hello, World!"
+    $scope.data_list = response.data.list;
+  });
 });
 
 
+TaskListApp.controller('TaskAddCtrl', function TaskAddController($scope, $http, $cookies) {
+  $scope.add = function(add_task, AddTaskForm) {
+    if(AddTaskForm.$valid){
+      $http.get("/get_csrf/", add_task).then(function success (response) {
+//        $scope.csrf_token_value=response.data;
+        var req = {
+          method: 'POST',
+          url: '/add_data_json/',
+          headers: {
+            'X-CSRFToken': $cookies.get('csrftoken'),
+          },
+          data: $scope.add_task,
+        }
+        $http(req).then(function(response_success){console.log('2xx - Все хорошо:\n', response_success);}, function(response_error){console.log('4xx - error response:\n', response_error);});
+      });
+    }
+  };
+});
--- task_list/app_task_list/views.py
+++ task_list/app_task_list/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render, render_to_response
 from django.http import JsonResponse
+from django.template.base import Template
+from django.http.response import HttpResponse
+from django.template.context import Context
+from django.middleware import csrf
+from django.http import Http404
+import json
 
 
 def main_page(request):
     return render_to_response("index.html")
+
+
+def get_csrf(request):
+    t = Template("{{csrf_token_value}}")
+    resp = t.render(Context({'csrf_token_value': csrf.get_token(request)}))
+    return HttpResponse(resp)
 
 
 def get_data_json(request):
@@ -27,3 +39,13 @@ def get_data_json(request):
     ]
 
     return JsonResponse({'list': data})
+
+
+def add_data_json(request):
+    if request.method == "POST" \
+            and request.content_type == 'application/json':
+        data_dict = json.loads(request.body.decode())
+
+        return render_to_response("index.html")
+    else:
+        raise Http404("Not POST or no data.")
