8e5067f0ecd0c17b3c9c270e8377c1318845fed6
ivangolubykh <ivan9@allworld.xyz>
2017-08-25 19:00:43 +0300

    Создана модель базы данных для хранения задач, произведена миграция базы данных. Тип поля для текста задачи изменён на textarea.

--- Docker/mysql/start_data/task_list-databases.sql
+++ Docker/mysql/start_data/task_list-databases.sql
@@ -16,6 +16,35 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `app_task_list_task`
+--
+
+DROP TABLE IF EXISTS `app_task_list_task`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `app_task_list_task` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `datetime_create` datetime NOT NULL,
+  `datetime_update` datetime NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `text` longtext NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `app_task_list_task_datetime_create_6155480f` (`datetime_create`),
+  KEY `app_task_list_task_datetime_update_c0571717` (`datetime_update`),
+  KEY `app_task_list_task_name_de37a87b` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `app_task_list_task`
+--
+
+LOCK TABLES `app_task_list_task` WRITE;
+/*!40000 ALTER TABLE `app_task_list_task` DISABLE KEYS */;
+/*!40000 ALTER TABLE `app_task_list_task` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `app_task_list_user`
 --
 
@@ -142,8 +171,8 @@ CREATE TABLE `auth_group_permissions` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `auth_group_permissions_group_id_permission_id_0cd325b0_uniq` (`group_id`,`permission_id`),
   KEY `auth_group_permissio_permission_id_84c5c92e_fk_auth_perm` (`permission_id`),
-  CONSTRAINT `auth_group_permissio_permission_id_84c5c92e_fk_auth_perm` FOREIGN KEY (`permission_id`) REFERENCES `auth_permission` (`id`),
-  CONSTRAINT `auth_group_permissions_group_id_b120cbf9_fk_auth_group_id` FOREIGN KEY (`group_id`) REFERENCES `auth_group` (`id`)
+  CONSTRAINT `auth_group_permissions_group_id_b120cbf9_fk_auth_group_id` FOREIGN KEY (`group_id`) REFERENCES `auth_group` (`id`),
+  CONSTRAINT `auth_group_permissio_permission_id_84c5c92e_fk_auth_perm` FOREIGN KEY (`permission_id`) REFERENCES `auth_permission` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -171,7 +200,7 @@ CREATE TABLE `auth_permission` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `auth_permission_content_type_id_codename_01ab375a_uniq` (`content_type_id`,`codename`),
   CONSTRAINT `auth_permission_content_type_id_2f476e4b_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -180,7 +209,7 @@ CREATE TABLE `auth_permission` (
 
 LOCK TABLES `auth_permission` WRITE;
 /*!40000 ALTER TABLE `auth_permission` DISABLE KEYS */;
-INSERT INTO `auth_permission` VALUES (1,'Can add log entry',1,'add_logentry'),(2,'Can change log entry',1,'change_logentry'),(3,'Can delete log entry',1,'delete_logentry'),(4,'Can add permission',2,'add_permission'),(5,'Can change permission',2,'change_permission'),(6,'Can delete permission',2,'delete_permission'),(7,'Can add group',3,'add_group'),(8,'Can change group',3,'change_group'),(9,'Can delete group',3,'delete_group'),(10,'Can add content type',4,'add_contenttype'),(11,'Can change content type',4,'change_contenttype'),(12,'Can delete content type',4,'delete_contenttype'),(13,'Can add session',5,'add_session'),(14,'Can change session',5,'change_session'),(15,'Can delete session',5,'delete_session'),(16,'Can add user',6,'add_user'),(17,'Can change user',6,'change_user'),(18,'Can delete user',6,'delete_user');
+INSERT INTO `auth_permission` VALUES (1,'Can add log entry',1,'add_logentry'),(2,'Can change log entry',1,'change_logentry'),(3,'Can delete log entry',1,'delete_logentry'),(4,'Can add permission',2,'add_permission'),(5,'Can change permission',2,'change_permission'),(6,'Can delete permission',2,'delete_permission'),(7,'Can add group',3,'add_group'),(8,'Can change group',3,'change_group'),(9,'Can delete group',3,'delete_group'),(10,'Can add content type',4,'add_contenttype'),(11,'Can change content type',4,'change_contenttype'),(12,'Can delete content type',4,'delete_contenttype'),(13,'Can add session',5,'add_session'),(14,'Can change session',5,'change_session'),(15,'Can delete session',5,'delete_session'),(16,'Can add user',6,'add_user'),(17,'Can change user',6,'change_user'),(18,'Can delete user',6,'delete_user'),(19,'Can add task',7,'add_task'),(20,'Can change task',7,'change_task'),(21,'Can delete task',7,'delete_task');
 /*!40000 ALTER TABLE `auth_permission` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -203,8 +232,8 @@ CREATE TABLE `django_admin_log` (
   PRIMARY KEY (`id`),
   KEY `django_admin_log_content_type_id_c4bce8eb_fk_django_co` (`content_type_id`),
   KEY `django_admin_log_user_id_c564eba6_fk_app_task_list_user_id` (`user_id`),
-  CONSTRAINT `django_admin_log_user_id_c564eba6_fk_app_task_list_user_id` FOREIGN KEY (`user_id`) REFERENCES `app_task_list_user` (`id`),
-  CONSTRAINT `django_admin_log_content_type_id_c4bce8eb_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`)
+  CONSTRAINT `django_admin_log_content_type_id_c4bce8eb_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`),
+  CONSTRAINT `django_admin_log_user_id_c564eba6_fk_app_task_list_user_id` FOREIGN KEY (`user_id`) REFERENCES `app_task_list_user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -230,7 +259,7 @@ CREATE TABLE `django_content_type` (
   `model` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `django_content_type_app_label_model_76bd3d3b_uniq` (`app_label`,`model`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -239,7 +268,7 @@ CREATE TABLE `django_content_type` (
 
 LOCK TABLES `django_content_type` WRITE;
 /*!40000 ALTER TABLE `django_content_type` DISABLE KEYS */;
-INSERT INTO `django_content_type` VALUES (1,'admin','logentry'),(6,'app_task_list','user'),(3,'auth','group'),(2,'auth','permission'),(4,'contenttypes','contenttype'),(5,'sessions','session');
+INSERT INTO `django_content_type` VALUES (1,'admin','logentry'),(7,'app_task_list','task'),(6,'app_task_list','user'),(3,'auth','group'),(2,'auth','permission'),(4,'contenttypes','contenttype'),(5,'sessions','session');
 /*!40000 ALTER TABLE `django_content_type` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -256,7 +285,7 @@ CREATE TABLE `django_migrations` (
   `name` varchar(255) NOT NULL,
   `applied` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -265,7 +294,7 @@ CREATE TABLE `django_migrations` (
 
 LOCK TABLES `django_migrations` WRITE;
 /*!40000 ALTER TABLE `django_migrations` DISABLE KEYS */;
-INSERT INTO `django_migrations` VALUES (1,'contenttypes','0001_initial','2017-08-25 15:35:24'),(2,'contenttypes','0002_remove_content_type_name','2017-08-25 15:35:25'),(3,'auth','0001_initial','2017-08-25 15:35:25'),(4,'auth','0002_alter_permission_name_max_length','2017-08-25 15:35:25'),(5,'auth','0003_alter_user_email_max_length','2017-08-25 15:35:25'),(6,'auth','0004_alter_user_username_opts','2017-08-25 15:35:25'),(7,'auth','0005_alter_user_last_login_null','2017-08-25 15:35:25'),(8,'auth','0006_require_contenttypes_0002','2017-08-25 15:35:25'),(9,'auth','0007_alter_validators_add_error_messages','2017-08-25 15:35:25'),(10,'auth','0008_alter_user_username_max_length','2017-08-25 15:35:25'),(11,'app_task_list','0001_initial','2017-08-25 15:35:25'),(12,'admin','0001_initial','2017-08-25 15:35:25'),(13,'admin','0002_logentry_remove_auto_add','2017-08-25 15:35:25'),(14,'sessions','0001_initial','2017-08-25 15:35:25');
+INSERT INTO `django_migrations` VALUES (1,'contenttypes','0001_initial','2017-08-25 15:35:24'),(2,'contenttypes','0002_remove_content_type_name','2017-08-25 15:35:25'),(3,'auth','0001_initial','2017-08-25 15:35:25'),(4,'auth','0002_alter_permission_name_max_length','2017-08-25 15:35:25'),(5,'auth','0003_alter_user_email_max_length','2017-08-25 15:35:25'),(6,'auth','0004_alter_user_username_opts','2017-08-25 15:35:25'),(7,'auth','0005_alter_user_last_login_null','2017-08-25 15:35:25'),(8,'auth','0006_require_contenttypes_0002','2017-08-25 15:35:25'),(9,'auth','0007_alter_validators_add_error_messages','2017-08-25 15:35:25'),(10,'auth','0008_alter_user_username_max_length','2017-08-25 15:35:25'),(11,'app_task_list','0001_initial','2017-08-25 15:35:25'),(12,'admin','0001_initial','2017-08-25 15:35:25'),(13,'admin','0002_logentry_remove_auto_add','2017-08-25 15:35:25'),(14,'sessions','0001_initial','2017-08-25 15:35:25'),(15,'app_task_list','0002_task','2017-08-25 15:59:06');
 /*!40000 ALTER TABLE `django_migrations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -303,4 +332,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-08-25 15:35:33
+-- Dump completed on 2017-08-25 15:59:10
